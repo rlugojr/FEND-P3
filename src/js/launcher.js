6576db28052860188f8363658709cb0574bbcb87
Ray Lugo, Jr <rlugojr@gmail.com>
2016-07-22 23:31:27 -0400

    Preloader
    Adding preloader to start screen to help have assets ready or loading
    for the main asset loader in loader.js.

--- src/js/launcher.js
+++ src/js/launcher.js
@@ -11,4 +11,18 @@ var launchScreen = (function(global) {
         volume: 0.25
     });
 
+//Help the loader by preloading while the user reads the help or looks at the start screen
+
+assetList = ['images/_textures/spritesheet.png',
+            'images/outro/win_screen.jpg',
+            'images/outro/lose_screen.jpg',
+            'images/effects/explosion.png',
+            'images/effects/bang.png',
+            'images/effects/level_up.png',
+            'images/effects/pow.png',
+            'images/effects/yeah.png'
+            ];
+
+
+
 })(this);
